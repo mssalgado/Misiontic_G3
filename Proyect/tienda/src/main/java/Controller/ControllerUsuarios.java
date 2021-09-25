adbc8a00defb63bd4124293188a086b1bed18781
jorgehd03 <jorgehernandezdelvalle.03@gmail.com>
2021-09-24 20:20:45 -0500

    Update ControllerUsuarios.java
    Se actualizó el ControllerUsuarios.

--- Proyect/tienda/src/main/java/Controller/ControllerUsuarios.java
+++ Proyect/tienda/src/main/java/Controller/ControllerUsuarios.java
@@ -1,7 +1,31 @@
+package controller;
 
-package Controller;
-
+import dao.DAOUsuarios;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import to.TOUsuarios;
 
 public class ControllerUsuarios {
-    
+
+    DAOUsuarios usuariosDAO = new DAOUsuarios();
+
+    public int insertarUsuario(TOUsuarios ToUsuario) {
+
+        return usuariosDAO.insertarUsuario(ToUsuario);
+    }
+
+    public boolean actualizarUsuario(TOUsuarios ToUsuario) {
+
+        return usuariosDAO.actualizarUsuario(ToUsuario);
+    }
+
+    public boolean eliminarUsuario(int idUsuario) {
+
+        return usuariosDAO.eliminarUsuario(idUsuario);
+    }
+
+    public ArrayList<TOUsuarios> listarUsuarios() {
+        return usuariosDAO.listarUsuarios();
+    }
 }
